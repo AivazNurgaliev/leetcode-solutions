7b6588da67fd515fb4a8b0d9f949b1018c93e3e9
AivazNurgaliev <aivaznurgaliev@gmail.com>
2023-02-03 20:31:26 +0500

    278 task

---
+++ src/easy/task_278.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class task_278 {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        int middle = 0;
+        while (left <= right) {
+            middle = left + (right - left) / 2;
+
+            if (isBadVersion(middle)) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return left;
+    }
+}
