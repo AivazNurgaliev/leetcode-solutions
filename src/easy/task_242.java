5fb64ecd89f26f3971890e6a22222bf50b3b9f60
AivazNurgaliev <aivaznurgaliev@gmail.com>
2023-03-07 16:33:13 +0300

    enhanced task_242

--- src/easy/task_242.java
+++ src/easy/task_242.java
@@ -1,17 +1,42 @@
 package easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class task_242 {
 
     public static void main(String[] args) {
-        String s = "anagram";
-        String t = "nagaram";
+        String s = "nl";
+        String t = "cx";
         System.out.println(isAnagram(s, t));
     }
 
     public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isAnagram1(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
